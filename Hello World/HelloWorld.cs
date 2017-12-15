89b20d0453b19d33cb1004df311659b727681013
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2017-12-15 11:51:53 -0500

    Add files via upload

---
+++ Hello World/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+	class HelloWorld
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World"); //writes out to the console
+
+			Console.WriteLine("Press any key to exit."); //keeps the window open until a key is pressed
+			Console.ReadKey();
+		}
+	}
+}
