311b617c89795610bd3c452a007ae10787612342
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2017-12-15 11:52:49 -0500

    Add files via upload

---
+++ Calculator/CalcFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	public static class CalcFunctions
+	{
+		public static int addFunction(int value1, int value2)
+		{
+			return value1 + value2;
+		}
+
+		public static int minusFunction(int value1, int value2)
+		{
+			return value1 - value2;
+		}
+
+		public static int multipleFunction(int value1, int value2)
+		{
+			return value1 * value2;
+		}
+
+		public static int divideFunction(int value1, int value2)
+		{
+			return value1 / value2;
+		}
+	}
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class Calculator
+	{
+		static void Main(string[] args)
+		{
+			bool exit = false;
+
+			while (!exit)
+			{
+				Console.WriteLine("Enter the first value:");
+				String value1 = Console.ReadLine();
+
+				Console.WriteLine("Enter the operator:");
+				String op = Console.ReadLine();
+
+				Console.WriteLine("Enter the second value:");
+				String value2 = Console.ReadLine();
+
+				Console.WriteLine(Calculate(value1, value2, op));
+
+				Console.WriteLine("Do you have another calculation (y/n):");
+				String repeat = Console.ReadLine();
+
+				if (repeat.Equals("n")) {
+					exit = true;
+				}
+			}
+		}
+
+		private static String Calculate(String value1, String value2, String op)
+		{
+			String res = null;
+			int? total = null;
+
+			switch (op)
+			{
+				case "+":
+					total = CalcFunctions.addFunction(Int32.Parse(value1), Int32.Parse(value2));
+					break;
+				case "-":
+					total = CalcFunctions.minusFunction(Int32.Parse(value1), Int32.Parse(value2));
+					break;
+				case "*":
+					total = CalcFunctions.multipleFunction(Int32.Parse(value1), Int32.Parse(value2));
+					break;
+				case "/":
+					total = CalcFunctions.divideFunction(Int32.Parse(value1), Int32.Parse(value2));
+					break;
+				default:
+					total = null;
+					break;
+			}
+
+			if (total != null)
+			{
+				res = value1 + " " + op + " " + value2 + " = " + total;
+			}
+			else
+			{
+				res = null;
+			}
+			return res;
+		}
+	}
+}
